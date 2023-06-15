064b894ea617e804a020550e1031b1cd1df1feac
Enzo Joaquin Bolzon <joaco.bolzon3@gmail.com>
2023-06-15 19:44:56 -0300

    Update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -62,7 +62,9 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
             >
+               <a href={"mailto:joacobolzon3@gmail.com"}>
               <button className="btn btn-lg">Contact me</button>
+              </a>
               <a href="#" className="text-gradient btn-link">
                 My Portfolio
               </a>
