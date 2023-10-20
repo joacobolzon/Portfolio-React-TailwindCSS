30ce623027a7d0f7bcbf27a75ee762cb1e1e277a
joacobolzon <joaco.bolzon3@gmail.com>
2023-10-20 13:54:40 -0300

    emailJS added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,36 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 //motion
 import { motion } from "framer-motion";
 //variants
 import { fadeIn } from "../variants";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uemwgfv",
+        "template_wmtxowf",
+        form.current,
+        "UyB92wm2rzdtGV0Qm"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setTimeout(() => {
+            window.alert("E-Mail enviado");
+            window.location.href = "https://portfolio-joacobolzon.vercel.app/";
+          }, 2000);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section className="py-16 lg:section" id="contact">
       <div className="container mx-auto">
@@ -29,6 +54,8 @@ const Contact = () => {
           </motion.div>
           {/* form */}
           <motion.form
+            ref={form}
+            onSubmit={sendEmail}
             variants={fadeIn("left", 0.3)}
             initial="hidden"
             whileInView={"show"}
@@ -36,16 +63,19 @@ const Contact = () => {
             className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start"
           >
             <input
+              name="user_name"
               type="text"
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               placeholder="Your name"
             />
             <input
+              name="user_email"
               type="text"
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               placeholder="Your E-Mail"
             />
             <textarea
+              name="message"
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
               placeholder="Your message"
             />
